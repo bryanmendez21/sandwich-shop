7b7d9301ee1f310c24dbb95223608fc4de7516d9
bryan <bryanmendez21@icloud.com>
2026-04-09 15:08:38 -0400

    added extra charge for loaded food. restructured if statements so they won't repeat

--- src/main/java/com/pluralsight/sandwich_shop.java
+++ src/main/java/com/pluralsight/sandwich_shop.java
@@ -4,56 +4,62 @@ import java.util.Scanner;
 
 public class sandwich_shop {
     public static void main(String[] args) {
-        // Variables
-        double regular = 5.45;
-        double large = 8.95;
+            // Variables
+            double regular = 5.45;
+            double large = 8.95;
+            double base_price = 0;
+            double loaded_price_regular = 1.0;
+            double loaded_price_large = 1.75;
+            double discount = 1.0;
 
-        // Inputs
-        Scanner shop_questions = new Scanner(System.in);
+            // Inputs
+            Scanner shop_questions = new Scanner(System.in);
 
-        System.out.println("Which size sandwich would you like (1) REGULAR: $5.45 or (2) Large: $8.95 input number: ");
-        int size_chosen = shop_questions.nextInt();
+            System.out.println("Which size sandwich would you like (1) REGULAR: $5.45 or (2) Large: $8.95 input number: ");
+            int size_chosen = shop_questions.nextInt();
 
-        System.out.println("How old are you: ");
-        int age = shop_questions.nextInt();
+            System.out.println("How old are you: ");
+            int age = shop_questions.nextInt();
+            shop_questions.nextLine();
 
-        // Logic
-        if (size_chosen == 1){
-            // Regular logic
+            System.out.println("do you want your sandwich loaded? (yes/no): ");
+            String loaded = shop_questions.nextLine();
+
             if (age <= 17){
-                double price_17and_younger = regular * .90;
-                System.out.println("price is: " + price_17and_younger);
-
+                discount = 0.90;
             }
             else if (age >= 65){
-                double price_65and_older = regular * .80;
-                System.out.println("price is: " + price_65and_older);
+                discount = 0.80;
             }
-            else{
-                System.out.println("price is: " + regular);
-            }
-        }
-        else if (size_chosen == 2){
-            // Large logic
-            if (age <= 17){
-                double price_17and_younger = large * .90;
-                System.out.println("price is: " + price_17and_younger);
 
-            }
-            else if (age >= 65){
-                double price_65and_older = large * .80;
-                System.out.println("price is: " + price_65and_older);
-            }
-            else{
-                System.out.println("price is: " + large);
-            }
-        }
-        else{
-            System.out.println("Choose between 1 and 2");
-        }
+            // Logic
+            if (size_chosen == 1) {
+                // Regular logic
+                base_price = regular;
 
 
+                if (loaded.equalsIgnoreCase("yes")){
+                    base_price += loaded_price_regular;
+                }
 
+                double final_price = base_price * discount;
+                System.out.printf("Price is: %.2f" ,final_price);
+            }
+            else if (size_chosen == 2) {
+                // Large logic
+                base_price = large;
+
+                if (loaded.equalsIgnoreCase("yes")){
+                    base_price += loaded_price_large;
+                }
+
+                double final_price = base_price * discount;
+                System.out.printf("Price is: %.2f" ,final_price);
+            }
+
+            else {
+                System.out.println("Choose between 1 and 2");
+            }
 
 
     }
